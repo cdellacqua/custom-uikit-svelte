b43676cfc7125d3e069da62f771ceecae0068a4f
adriana <adriferra97@gmail.com>
2020-09-05 22:12:14 +0200

    add accordion component

---
+++ src/components/accordion/Accordion.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let activeIndex = false;
+  export let duration = 200;
+  export let multi = false;
+  export let collapsible = true;
+</script>
+
+<ul uk-accordion="multiple: {multi}; collapsible: {collapsible}; duration: {duration}; active: {activeIndex}">
+  <slot />
+</ul>
---
+++ src/components/accordion/AccordionItem.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let title = "";
+  export let open = false;
+</script>
+
+<li class:uk-open={open}>
+  <a class="uk-accordion-title" href="#">{title}</a>
+  <div class="uk-accordion-content">
+    <slot />
+  </div>
+</li>
