e8e8d3ffcf4f1bbf2402cbb5ca1d7b2489f5e9bf
Carlo Dell'Acqua <carlo.dellacqua97@gmail.com>
2020-09-06 17:37:17 +0200

    remove # from breadcrumb path

--- src/components/Breadcrumb.svelte
+++ src/components/Breadcrumb.svelte
@@ -5,7 +5,7 @@
 
 <ul class="uk-margin-small-left uk-margin-small-top uk-margin-small-bottom uk-breadcrumb {className}">
   {#each path.slice(0, -1) as entry}
-    <li><a href={"#" + entry.href}>{entry.label}</a></li>
+    <li><a href={entry.href}>{entry.label}</a></li>
   {/each}
   {#if path.length > 0}
     <li><span>{path[path.length - 1].label}</span></li>
