d69c420221917c750b5ef7284fa68b0f6adbfc2f
Carlo Dell'Acqua <carlo.dellacqua97@gmail.com>
2020-09-06 18:42:27 +0200

    accordion title support html

--- src/components/accordion/AccordionItem.svelte
+++ src/components/accordion/AccordionItem.svelte
@@ -1,11 +1,16 @@
 <script>
   export let title = "";
+  export let isTitleHtml = false;
   export let open = false;
 </script>
 
 <li class:uk-open={open}>
   <!-- svelte-ignore a11y-missing-attribute -->
-  <a class="uk-accordion-title" role="button">{title}</a>
+  <a class="uk-accordion-title" role="button">
+    {#if isTitleHtml}
+      {@html title}
+    {:else}{title}{/if}
+  </a>
   <div class="uk-accordion-content">
     <slot />
   </div>
