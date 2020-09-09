c98372f6a7bcfb8e842b3d97bb754cc1a33c960e
adrianaferrari <adriana.ferrari997@gmail.com>
2020-09-09 23:34:29 +0200

    dev script; accordion demo

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,17 @@
 <script>
+import AccordionDemo from "./demo/AccordionDemo.svelte";
+
 import { Accordion, AccordionItem, Radio, FixedPointInput, Form, NumberInput } from "./main";
 
 </script>
 
-<Accordion multi>
-	<AccordionItem title="Accordion item 1">
-		<p>Accordion content 1</p>
-	</AccordionItem>
-	<AccordionItem title="Accordion item 2">
-		<p>Accordion content 2</p>
-	</AccordionItem>
-</Accordion>
+<div uk-grid class="uk-flex-center uk-padding-small">
+	<div class="uk-width-3-5@l uk-width-2-3@m uk-width-5-6@s uk-width-1-1">
+		<h2 class="uk-heading-divider">Accordion</h2>
+		<AccordionDemo  />
+	</div>
+</div>
+
 
 <Radio label={"test"} options={[{label: 'opz1', value: 1}, {label: 'opz2', value: 2}, {label: 'opz3', value: 3}]} />
 
--- src/components/accordion/Accordion.svelte
+++ src/components/accordion/Accordion.svelte
@@ -6,8 +6,10 @@
   export let multi = false;
   export let transition = "ease";
   export let ref = undefined;
+  export let className = undefined;
+  export let style = undefined;
 </script>
 
-<ul bind:this={ref} uk-accordion="multiple: {multi}; collapsible: {collapsible}; duration: {duration}; active: {activeIndex}; transition: {transition}; animation: {animation}">
+<ul class={className} {style} bind:this={ref} uk-accordion="multiple: {multi}; collapsible: {collapsible}; duration: {duration}; active: {activeIndex}; transition: {transition}; animation: {animation}">
   <slot />
 </ul>
