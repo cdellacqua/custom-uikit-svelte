3bf8ef3eb0234e632bf13582b7d1247ed87ba2c8
Carlo Dell'Acqua <carlo.dellacqua97@gmail.com>
2020-09-19 17:42:51 +0200

    improve loader overlay

--- src/components/LoaderOverlay.svelte
+++ src/components/LoaderOverlay.svelte
@@ -1,11 +1,12 @@
 <script>
   import { onMount, onDestroy } from "svelte";
-import Loader from "./Loader.svelte";
+  import Loader from "./Loader.svelte";
 
   export let className = undefined;
   export let ratio = 1;
   export let style = undefined;
   export let ref = undefined;
+  export let opacity = 0.8;
 
   let originalWrapperRef;
   onMount(() => {
@@ -19,7 +20,7 @@ import Loader from "./Loader.svelte";
   });
 </script>
 
-<style>
+<style lang="scss">
   .overlay {
     position: fixed;
     top: 0;
@@ -27,11 +28,25 @@ import Loader from "./Loader.svelte";
     right: 0;
     bottom: 0;
     z-index: 999999999;
+    > div {
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+    }
   }
 </style>
 
 <div bind:this={originalWrapperRef}>
-  <div class="overlay" bind:this={ref}>
-    <Loader {className} {ratio} {style} />
+  <div
+    class="overlay"
+    bind:this={ref}
+    {className}
+    {style}>
+    <div class="uk-background-muted" style="opacity: {opacity}"></div>
+    <div class="uk-flex uk-flex-middle uk-flex-center">
+      <Loader {ratio} />
+    </div>
   </div>
 </div>
