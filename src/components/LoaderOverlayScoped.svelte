a550c66b3468e7d0f5bd42274ee85a25b3de2b33
Carlo Dell'Acqua <carlo.dellacqua97@gmail.com>
2020-10-15 18:36:48 +0200

    add scoped loader overlay

---
+++ src/components/LoaderOverlayScoped.svelte
@@ -0,0 +1,47 @@
+<script>
+  import Loader from "./Loader.svelte";
+
+  /** @type {string|undefined} */
+  export let className = undefined;
+  /** @type {number} */
+  export let ratio = 1;
+  /** @type {string|undefined} */
+  export let style = undefined;
+  /** @type {HTMLDivElement} */
+  export let ref = undefined;
+  /** @type {number} */
+  export let opacity = 0.8;
+  /** @type {boolean} */
+  export let loading = true;
+</script>
+
+<style lang="scss">
+  .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 999999999;
+    > div {
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+    }
+  }
+</style>
+
+{#if loading}
+  <div
+    class="overlay"
+    bind:this={ref}
+    {className}
+    {style}>
+    <div class="uk-background-muted" style="opacity: {opacity}"></div>
+    <div class="uk-flex uk-flex-middle uk-flex-center">
+      <Loader {ratio} />
+    </div>
+  </div>
+{/if}
