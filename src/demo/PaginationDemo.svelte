77da656901fb1847aef59d63cacb64e0c720b0c4
Carlo Dell'Acqua <carlo.dellacqua97@gmail.com>
2020-10-16 19:06:20 +0200

    add pagination demo

---
+++ src/demo/PaginationDemo.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { Button, NumberInput, Pagination, Switcher } from "../main";
+  
+  let numberOfPages = 0;
+  let numbersPerSide = 1;
+  let pageIndex = 0;
+  
+</script>
+
+<NumberInput bind:value={numberOfPages} label={'Number of pages'} />
+<NumberInput bind:value={numbersPerSide} label={'Numbers per side'} />
+<NumberInput bind:value={pageIndex} label={'Page index'} />
+
+<Pagination center {numberOfPages} {numbersPerSide} {pageIndex} />
