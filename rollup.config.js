6225c20ea20c49765665bec45912fb864f7aa2af
Carlo Dell'Acqua <carlo.dellacqua97@gmail.com>
2021-03-10 19:23:56 +0100

    add preferBuiltIns: false to rollup.config to prevent possible future problems with external libraries

--- rollup.config.js
+++ rollup.config.js
@@ -57,7 +57,8 @@ export default [{
 		// https://github.com/rollup/plugins/tree/master/packages/commonjs
 		resolve({
 			browser: true,
-			dedupe: ['svelte']
+			dedupe: ['svelte'],
+			preferBuiltins: false,
 		}),
 		commonjs(),
 	],
