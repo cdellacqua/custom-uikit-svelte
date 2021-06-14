9f9a2e10b7fc52e23d5078e9d50c8c981f1255b0
Carlo Dell'Acqua <carlo.dellacqua97@gmail.com>
2021-06-14 13:59:24 +0200

    fix rows reactivity on datatable

--- src/components/DataTable.svelte
+++ src/components/DataTable.svelte
@@ -1,153 +1,163 @@
 <script>
-  import { createEventDispatcher } from "svelte";
-  import { noop } from "../helpers/lambdas";
-  import AsyncDataTable from "./AsyncDataTable.svelte";
+	import { createEventDispatcher } from "svelte";
+	import { noop } from "../helpers/lambdas";
+	import AsyncDataTable from "./AsyncDataTable.svelte";
 
-  /** @type {Array<{label: string, key: string, className: string|undefined, textAlign: 'center'|'right'|'left'|undefined, orderable: boolean|Comparator|undefined, searchable: boolean|DataTableSearchCallback|undefined, render: DataTableRenderer|undefined}>} */
-  export let columns = [];
-  /** @type {Array<Record<string, any>>} */
-  export let rows = [];
-  /** @type {Array<Record<string, any>>} */
-  export let visibleRows = [];
-  /** @type {undefined|'small'} */
-  export let size = undefined;
-  /** @type {undefined|string} */
-  export let className = undefined;
-  /**
-   * @description A string specifying custom style properties for the component
-   * @type {string|undefined} */
-  export let style = undefined;
-  /** @type {'divider'|'striped'} */
-  export let appearance = "divider";
-  /** @type {'default'|'primary'|'secondary'|'danger'|'text'|'link'} */
-  export let searchButtonVariant = "default";
-  /** @type {boolean} */
-  export let stickyHeader = false;
-  /** @type {string} */
-  export let placeholder = "";
-  /** @type {string|undefined} */
-  export let noResultText = undefined;
-  /** @type {HTMLTableElement} */
-  export let ref = undefined;
-  /** @type {boolean} @default true */
-  export let instantSearch = true;
-  /** @type {string} */
-  export let query = "";
-  /** @type {Array<{key: string, direction: 'desc'|'asc'}>} */
-  export let orderBy = [];
-  /** @type {boolean} @default true */
-  export let horizontalScroll = true;
-  /** @type {number} */
-  export let recordsPerPage = 25;
-  /** @type {number} */
-  export let numbersPerSide = 4;
-  /** @type {number} */
-  export let pageIndex = 0;
-  /** @type {number} @readonly */
-  export let total = 0;
-  /** @type {number} @readonly */
-  export let filtered = 0;
+	/** @type {Array<{label: string, key: string, className: string|undefined, textAlign: 'center'|'right'|'left'|undefined, orderable: boolean|Comparator|undefined, searchable: boolean|DataTableSearchCallback|undefined, render: DataTableRenderer|undefined}>} */
+	export let columns = [];
+	/** @type {Array<Record<string, any>>} */
+	export let rows = [];
+	/** @type {Array<Record<string, any>>} */
+	export let visibleRows = [];
+	/** @type {undefined|'small'} */
+	export let size = undefined;
+	/** @type {undefined|string} */
+	export let className = undefined;
+	/**
+	 * @description A string specifying custom style properties for the component
+	 * @type {string|undefined} */
+	export let style = undefined;
+	/** @type {'divider'|'striped'} */
+	export let appearance = "divider";
+	/** @type {'default'|'primary'|'secondary'|'danger'|'text'|'link'} */
+	export let searchButtonVariant = "default";
+	/** @type {boolean} */
+	export let stickyHeader = false;
+	/** @type {string} */
+	export let placeholder = "";
+	/** @type {string|undefined} */
+	export let noResultText = undefined;
+	/** @type {HTMLTableElement} */
+	export let ref = undefined;
+	/** @type {boolean} @default true */
+	export let instantSearch = true;
+	/** @type {string} */
+	export let query = "";
+	/** @type {Array<{key: string, direction: 'desc'|'asc'}>} */
+	export let orderBy = [];
+	/** @type {boolean} @default true */
+	export let horizontalScroll = true;
+	/** @type {number} */
+	export let recordsPerPage = 25;
+	/** @type {number} */
+	export let numbersPerSide = 4;
+	/** @type {number} */
+	export let pageIndex = 0;
+	/** @type {number} @readonly */
+	export let total = 0;
+	/** @type {number} @readonly */
+	export let filtered = 0;
 
-  const dispatch = createEventDispatcher();
+	const dispatch = createEventDispatcher();
 
-  function fallbackComparator(a, b) {
-    if (a < b) {
-      return -1;
-    }
-    if (a > b) {
-      return 1;
-    }
-    return 0;
-  }
+	function fallbackComparator(a, b) {
+		if (a < b) {
+			return -1;
+		}
+		if (a > b) {
+			return 1;
+		}
+		return 0;
+	}
 
-  function dataProvider(query, orderBy, recordsPerPage, pageIndex) {
-    const filteredRows = rows.filter((row) =>
-      columns.some((col) => {
-        if (col.searchable === false) {
-          return query.length === 0;
-        }
-        if (typeof col.searchable === "function") {
-          return col.searchable(query, row[col.key], row);
-        }
-        if (!col.render) {
-          return String(row[col.key])
-            .toLowerCase()
-            .includes(query.toLowerCase());
-        }
-        const output = col.render(row[col.key], row);
-        if (typeof output === "string") {
-          return output.toLowerCase().includes(query.toLowerCase());
-        }
-        return false;
-      })
-    );
+	function dataProvider(query, orderBy, recordsPerPage, pageIndex) {
+		const filteredRows = rows.filter((row) =>
+			columns.some((col) => {
+				if (col.searchable === false) {
+					return query.length === 0;
+				}
+				if (typeof col.searchable === "function") {
+					return col.searchable(query, row[col.key], row);
+				}
+				if (!col.render) {
+					return String(row[col.key])
+						.toLowerCase()
+						.includes(query.toLowerCase());
+				}
+				const output = col.render(row[col.key], row);
+				if (typeof output === "string") {
+					return output.toLowerCase().includes(query.toLowerCase());
+				}
+				return false;
+			})
+		);
 
-    if (orderBy.length > 0) {
-      const columnsByKey = columns.reduce((a, c) => {
-        a[c.key] = c;
-        return a;
-      }, {});
-      filteredRows.sort((r1, r2) => {
-        for (const columnSort of orderBy) {
-          let comparison = 0;
-          if (typeof columnsByKey[columnSort.key].orderable === "function") {
-            comparison =
-              columnsByKey[columnSort.key].orderable(
-                r1[columnSort.key],
-                r2[columnSort.key]
-              ) * (columnSort.direction === "asc" ? 1 : -1);
-          } else {
-            comparison =
-              fallbackComparator(r1[columnSort.key], r2[columnSort.key]) *
-              (columnSort.direction === "asc" ? 1 : -1);
-          }
-          if (comparison !== 0) {
-            return comparison;
-          }
-        }
-        return 0;
-      });
-    }
-    return {
-      total: rows.length,
-      filtered: filteredRows.length,
-      records: filteredRows.slice(
-        pageIndex * recordsPerPage,
-        (pageIndex + 1) * recordsPerPage
-      ),
-    };
-  }
+		if (orderBy.length > 0) {
+			const columnsByKey = columns.reduce((a, c) => {
+				a[c.key] = c;
+				return a;
+			}, {});
+			filteredRows.sort((r1, r2) => {
+				for (const columnSort of orderBy) {
+					let comparison = 0;
+					if (typeof columnsByKey[columnSort.key].orderable === "function") {
+						comparison =
+							columnsByKey[columnSort.key].orderable(
+								r1[columnSort.key],
+								r2[columnSort.key]
+							) * (columnSort.direction === "asc" ? 1 : -1);
+					} else {
+						comparison =
+							fallbackComparator(r1[columnSort.key], r2[columnSort.key]) *
+							(columnSort.direction === "asc" ? 1 : -1);
+					}
+					if (comparison !== 0) {
+						return comparison;
+					}
+				}
+				return 0;
+			});
+		}
+		return {
+			total: rows.length,
+			filtered: filteredRows.length,
+			records: filteredRows.slice(
+				pageIndex * recordsPerPage,
+				(pageIndex + 1) * recordsPerPage
+			),
+		};
+	}
 
-  $: if (orderBy) {
-    dispatch("sort", orderBy);
-  }
+	$: if (orderBy) {
+		dispatch("sort", orderBy);
+	}
+
+	let asyncDataTable;
+	function reload() {
+		if (!asyncDataTable) {
+			return;
+		}
+		asyncDataTable.reload();
+	}
+	$: rows, reload();
 </script>
 
 <AsyncDataTable
-  {appearance}
-  on:query
-  on:row-click
-  on:row-dblclick
-  {className}
-  {columns}
-  {dataProvider}
-  debounceMs={0}
-  bind:filtered
-  {horizontalScroll}
-  {instantSearch}
-  dataProviderErrorHandler={noop}
-  {noResultText}
-  {numbersPerSide}
-  bind:orderBy
-  bind:pageIndex
-  {placeholder}
-  bind:query
-  {recordsPerPage}
-  bind:ref
-  bind:rows={visibleRows}
-  {searchButtonVariant}
-  {size}
-  {stickyHeader}
-  {style}
-  bind:total
+	{appearance}
+	on:query
+	on:row-click
+	on:row-dblclick
+	{className}
+	{columns}
+	{dataProvider}
+	debounceMs={0}
+	bind:filtered
+	{horizontalScroll}
+	{instantSearch}
+	dataProviderErrorHandler={noop}
+	{noResultText}
+	{numbersPerSide}
+	bind:orderBy
+	bind:pageIndex
+	{placeholder}
+	bind:query
+	{recordsPerPage}
+	bind:ref
+	bind:rows={visibleRows}
+	{searchButtonVariant}
+	{size}
+	{stickyHeader}
+	{style}
+	bind:total
+	bind:this={asyncDataTable}
 />
--- src/demo/DataTableDemo.svelte
+++ src/demo/DataTableDemo.svelte
@@ -1,39 +1,48 @@
 <script>
-  import { Button, DataTable } from '../main';
+	import { Button, DataTable } from "../main";
 
-  let data = [{
-    column1: new Date(),
-    column2: 'A string',
-    someNumericValue: 10.1,
-  },{
-    column1: new Date("2020-07-06"),
-    column2: 'C string',
-    someNumericValue: null,
-  },{
-    column1: new Date("2020-05-01"),
-    column2: 'C string',
-    someNumericValue: null,
-  },{
-    column1: new Date("2020-01-11"),
-    column2: 'D string',
-    someNumericValue: 8,
-  },]
-
-  let columns = [{
-			label: 'Column 1',
-			key: 'column1',
-      render: (data) => data.toLocaleString(),
+	let data = [
+		{
+			column1: new Date(),
+			column2: "A string",
+			someNumericValue: 10.1,
 		},
 		{
-			label: 'Column 2',
-      key: 'column2',
-      searchable: (query, col, row) => {
-        return [row.column1.toLocaleString(), col].join(' ').toLowerCase().includes(query.toLowerCase());
-      }
+			column1: new Date("2020-07-06"),
+			column2: "C string",
+			someNumericValue: null,
 		},
 		{
-			label: 'Nullable numeric value',
-			key: 'someNumericValue',
+			column1: new Date("2020-05-01"),
+			column2: "C string",
+			someNumericValue: null,
+		},
+		{
+			column1: new Date("2020-01-11"),
+			column2: "D string",
+			someNumericValue: 8,
+		},
+	];
+
+	let columns = [
+		{
+			label: "Column 1",
+			key: "column1",
+			render: (data) => data.toLocaleString(),
+		},
+		{
+			label: "Column 2",
+			key: "column2",
+			searchable: (query, col, row) => {
+				return [row.column1.toLocaleString(), col]
+					.join(" ")
+					.toLowerCase()
+					.includes(query.toLowerCase());
+			},
+		},
+		{
+			label: "Nullable numeric value",
+			key: "someNumericValue",
 			orderable: (v1, v2) => {
 				if (v1 && v2) {
 					return v1 - v2;
@@ -42,32 +51,43 @@
 				if (v2) return 1;
 				return 0;
 			},
-      render: (n) => n,
+			render: (n) => n,
 		},
 		{
-			label: 'Button',
-			key: 'someNumericValue',
-      orderable: false,
-      searchable: false,
-      render: (n) => ({
-        component: Button,
-        props: {
-          variant: 'primary',
-        },
-        onClick: () => alert(n),
-        textContent: 'I\'m a button',
-      }),
-		}];
+			label: "Button",
+			key: "someNumericValue",
+			orderable: false,
+			searchable: false,
+			render: (n) => ({
+				component: Button,
+				props: {
+					variant: "primary",
+				},
+				onClick: () => alert(n),
+				textContent: "I'm a button",
+			}),
+		},
+	];
+
+	setTimeout(() => {
+		data = [
+			{
+				column1: new Date("2020-01-11"),
+				column2: "D string",
+				someNumericValue: 8,
+			},
+		];
+	}, 10000);
 </script>
 
 <DataTable
-  {columns}
-  rows={data}
-  recordsPerPage={2}
-  orderBy={[{key: 'column1', direction: 'asc'}]}
-  on:query={({ detail }) => console.log(detail)}
-  on:sort={({ detail }) => console.log(detail)}
-  instantSearch={false}
-  on:row-click={({ detail }) => console.log(detail)}
-  on:row-dblclick={({ detail }) => console.log('dbl-click')}
+	{columns}
+	rows={data}
+	recordsPerPage={2}
+	orderBy={[{ key: "column1", direction: "asc" }]}
+	on:query={({ detail }) => console.log(detail)}
+	on:sort={({ detail }) => console.log(detail)}
+	instantSearch={false}
+	on:row-click={({ detail }) => console.log(detail)}
+	on:row-dblclick={({ detail }) => console.log("dbl-click")}
 />
